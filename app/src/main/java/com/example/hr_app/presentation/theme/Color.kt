5dc484c80e3646adfa800d0aee89c6385115b63f
Boatswain <son4ck.2005@list.ru>
2026-05-03 17:25:31 +0300

    Created Color.kt in presentation.theme with primary, secondary, background, surface, error colors for light and dark themes.

---
+++ app/src/main/java/com/example/hr_app/presentation/theme/Color.kt
@@ -0,0 +1,25 @@
+package com.example.hr_app.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightPrimary = Color(0xFF2563EB)
+val LightSecondary = Color(0xFF10B981)
+val LightBackground = Color(0xFFFFFFFF)
+val LightSurface = Color(0xFFF9FAFB)
+val LightError = Color(0xFFEF4444)
+val LightOnPrimary = Color.White
+val LightOnSecondary = Color.White
+val LightOnBackground = Color(0xFF111827)
+val LightOnSurface = Color(0xFF374151)
+val LightOnError = Color.White
+
+val DarkPrimary = Color(0xFF3B82F6)
+val DarkSecondary = Color(0xFF34D399)
+val DarkBackground = Color(0xFF111827)
+val DarkSurface = Color(0xFF1F2937)
+val DarkError = Color(0xFFF87171)
+val DarkOnPrimary = Color.White
+val DarkOnSecondary = Color(0xFF062B22)
+val DarkOnBackground = Color(0xFFF9FAFB)
+val DarkOnSurface = Color(0xFFE5E7EB)
+val DarkOnError = Color(0xFF1F2937)
