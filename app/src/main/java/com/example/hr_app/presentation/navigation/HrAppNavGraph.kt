7db518222aaa2e8c6185c84f5d58f996e7fa98cd
Boatswain <son4ck.2005@list.ru>
2026-05-05 09:35:20 +0300

    Add RegisterScreen function

--- app/src/main/java/com/example/hr_app/presentation/navigation/HrAppNavGraph.kt
+++ app/src/main/java/com/example/hr_app/presentation/navigation/HrAppNavGraph.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.hr_app.presentation.screens.auth.LoginScreen
+import com.example.hr_app.presentation.screens.auth.RegisterScreen
 
 @Composable
 fun HrAppNavGraph(
